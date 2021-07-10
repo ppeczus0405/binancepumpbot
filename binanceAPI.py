b0e4b4b1f3da3bc3fccfda19160a491e18364fd5
Patryk Peczak <ppeczak0405@gmail.com>
2021-07-10 22:49:13 +0200

    Working on binance sell/buy with browser requests

--- binanceAPI.py
+++ binanceAPI.py
@@ -1,244 +1,118 @@
-import time
-import hashlib
 import requests
-import hmac
 import config
+import json
 from enum import Enum, auto
 from urllib.parse import urlencode
 
+
+class OrderType(Enum):
+    LIMIT = auto()
+    MARKET = auto()
+
+
+class OrderSide(Enum):
+    BUY = auto()
+    SELL = auto()
+
+
 class BinanceAPI:
     
-    BASE_URL = "https://api.binance.com/api/v1"
-    BASE_URL_V3 = "https://api.binance.com/api/v3"
+    BASE_URL = "https://www.binance.com"
+    BASE_API_URL = "https://api.binance.com/api/v3"
     COINS_SYMBOLS = frozenset()
     TICK_STEP_DICT = dict()
 
-    class OrderType(Enum):
-        LIMIT = auto()
-        MARKET_QUANTITY = auto()
-        MARKET_FOR = auto()
-
-    def __init__(self, key, secret):
+    def __init__(self, key, secret, timeout=20, verify=True):
+        self.req_timeout = timeout
+        self.req_verify = verify
         self.key = key
         self.secret = secret
-        BinanceAPI.COINS_SYMBOLS = frozenset(self._get_symbols_fill_filter())
+        self.headers = dict()
+        self.__fill_symbols_dicts()
+        self.__process_template(config.binance_header)
 
-    def get_open_orders_in_str(self):
-        l = []
-        index = 0
-        for order_dict in self._get("%s/openOrders" % (self.BASE_URL_V3)):
-            l.append("#%d: " % (index) + str(order_dict))
-            index = index + 1
-        return "\n".join(l)
+    def _get(self, url, params={}):
+        request_headers = dict()
+        request_headers.update(self.headers)
+        request_url = "%s?%s" % (url, urlencode(params))
+        return requests.get(request_url, headers=request_headers,
+                            verify=self.req_verify, timeout=self.req_timeout).json()
 
+    def _post(self, url, params={}):
+        request_headers = dict()
+        request_headers.update(self.headers)
+        return requests.post(url, headers=request_headers, json=json.dumps(params),
+                             verify=self.req_verify, timeout=self.req_timeout).json()
 
-    def get_symbol(self, first_coin, second_coin):
-        if first_coin + second_coin in self.COINS_SYMBOLS:
-            return first_coin + second_coin
-        elif second_coin + first_coin in self.COINS_SYMBOLS:
-            return second_coin + first_coin
-        return False
+    def market_order(self, symbol, side, size=0, funds=0):
+        if size > 0:
+            order_params = self._order(symbol, side, OrderType.MARKET, size=size)
+        else:
+            order_params = self._order(symbol, side, OrderType.MARKET, funds=funds)
+        url = "%s/%s" % (BinanceAPI.BASE_URL, "/bapi/mbx/v1/private/mbxgateway/order/place")
+        return self._post(url, order_params)
 
-    def cancel_all_open_orders(self, fcoin, scoin):
-        symbol = self.get_symbol(fcoin, scoin)
-        if symbol is not False:
-            self._delete("%s/openOrders" % (self.BASE_URL_V3), {"symbol" : symbol})
-            return True
-        return "Given pair of coins is not listed on binance"
-
-    def get_price(self, symbol):
-        if symbol in self.COINS_SYMBOLS:
-            return float(self._get_no_sign("%s/avgPrice" % (self.BASE_URL_V3), {"symbol" : symbol})['price'])
-        return -1.0
-    
-    def get_coin_amount(self, coin_name="BTC"):
-        path = "%s/account" % (self.BASE_URL_V3)
-        account = self._get(path)
-        for coin_dict in account["balances"]:
-            if coin_dict["asset"] == coin_name:
-                return float(coin_dict["free"])
-        return 0.0
-
-    def get_wallet(self):
-        l = []
-        account = self._get("%s/account" % (self.BASE_URL_V3))
-        for coin_dict in account["balances"]:
-            s = "%s: f: %s l: %s" % (coin_dict["asset"], coin_dict["free"], coin_dict["locked"])
-            l.append(s)
-        return l
-
-    # MARKET BUY BEGIN
-    def __buy_coin(self, bcoin, ammount, opcoin, ordertype):
-        if bcoin + opcoin in self.COINS_SYMBOLS:
-            params = self._order(bcoin + opcoin, ammount, "BUY", ordertype)
-            order = self._post("%s/order" % (self.BASE_URL_V3), params)
-
-            if order.get('msg') is not None:
-                return "Cannot buy %s for %s. Reason: %s" % (bcoin, opcoin, order['msg'])
-            expired_not_buyed = order.get('status') == "EXPIRED" and float(order['executedQty']) == 0.0
-            if order.get('status') not in ("PARTIALLY_FILLED", "FILLED", "EXPIRED") or expired_not_buyed:
-                return "Cannot buy %s for %s. Order status: %s" % (bcoin, opcoin, order['status'])
-        
-            return (float(order['executedQty']), bcoin, float(order['cummulativeQuoteQty']), opcoin)
-        elif opcoin + bcoin in self.COINS_SYMBOLS:
-            if ordertype == self.OrderType.MARKET_QUANTITY:
-                return self.sell_coin_for(opcoin, ammount, bcoin)
-            return self.sell_coin(opcoin, ammount, bcoin)
-        return "Cannot buy %s for %s. Reason: Given pair is not listed on Binance." % (bcoin, opcoin)
-
-    def buy_coin(self, bcoin, ammount, opcoin):
-        return self.__buy_coin(bcoin, ammount, opcoin, self.OrderType.MARKET_QUANTITY)
-    
-    def buy_coin_for(self, bcoin, ammount, opcoin):
-        return self.__buy_coin(bcoin, ammount, opcoin, self.OrderType.MARKET_FOR)
-    # MARKET BUY END
-
-    # MARKET SELL BEGIN
-    def __sell_coin(self, scoin, ammount, bcoin, ordertype):
-        if scoin + bcoin in self.COINS_SYMBOLS:
-            params = self._order(scoin + bcoin, ammount, "SELL", ordertype)
-            order = self._post("%s/order" % (self.BASE_URL_V3), params)
-
-            if order.get('msg') is not None:
-                return "Cannot sell %s for %s. Reason: %s" % (scoin, bcoin, order['msg'])
-            expired_not_selled = order.get('status') == "EXPIRED" and float(order['executedQty']) == 0.0
-            if order.get('status') not in ("PARTIALLY_FILLED", "FILLED", "EXPIRED") or expired_not_selled:
-                return "Cannot sell %s for %s. Order status: %s" % (scoin, bcoin, order['status'])
-            
-            return (float(order['cummulativeQuoteQty']), bcoin, float(order['executedQty']), scoin)
-        elif bcoin + scoin in self.COINS_SYMBOLS:
-            if ordertype == self.OrderType.MARKET_QUANTITY:
-                return self.buy_coin_for(bcoin, ammount, scoin)
-            return self.buy_coin(bcoin, ammount, scoin)
-        return "Cannot sell %s for %s. Reason: Given pair is not listed on Binance." % (scoin, bcoin)
-
-    def sell_coin(self, scoin, ammount, bcoin):
-        return self.__sell_coin(scoin, ammount, bcoin, self.OrderType.MARKET_QUANTITY)
-    
-    def sell_coin_for(self, scoin, ammount, bcoin):
-        return self.__sell_coin(scoin, ammount, bcoin, self.OrderType.MARKET_FOR)
-    # MARKET SELL END
-
-    # LIMIT ORDERS
-    def __limit_order(self, bcoin, ammount, scoin, price, side):
+    def _order(self, symbol, side, order_type, size=0, funds=0, price=0):
         params = dict()
-        trade_pair = ("buy", bcoin, scoin)
-        if bcoin + scoin in self.COINS_SYMBOLS:
-            if side == "BUY":
-                params = self._order(bcoin + scoin, ammount, "BUY", self.OrderType.LIMIT, price)
-            else:
-                params = self._order(bcoin + scoin, ammount * price, "BUY", self.OrderType.LIMIT, 1.0 / price)
-        elif scoin + bcoin in self.COINS_SYMBOLS:
-            if side == "SELL":
-                params = self._order(scoin + bcoin, ammount, "SELL", self.OrderType.LIMIT, price)
-            else:
-                params = self._order(scoin + bcoin, ammount * price, "SELL", self.OrderType.LIMIT, 1.0 / price)
-            trade_pair = ("sell", scoin, bcoin)
+        params['side'] = "BUY" if side == OrderSide.BUY else "SELL"
+        params['symbol'] = symbol
+        params['type'] = "MARKET"
+        size_tick, price_tick = BinanceAPI.TICK_STEP_DICT[symbol]
+
+        if funds > 0:
+            params["quoteOrderQty"] = "%0.8f" % funds
         else:
-            return "Cannot limit %s %s for %s. Reason: Given pair is not listed on Binance" % trade_pair
+            params['quantity'] = self._fix_value_to_pass_filters(size, size_tick)
 
-        order = self._post("%s/order" % (self.BASE_URL_V3), params)
-
-        if order.get('msg') is not None:
-            return "Cannot limit %s %s for %s. Reason: %s" % tuple(list(trade_pair) + [order['msg']])
-        order_status = order.get('status')
-        if order_status not in ("NEW", "PARTIALLY_FILLED", "FILLED"):
-            return "Cannot limit %s %s for %s. Order status: %s" % tuple(list(trade_pair) + [order['status']])
-        
-        return True
-    
-    def limit_buy(self, bcoin, ammount, opcoin, price):
-        return self.__limit_order(bcoin, ammount, opcoin, price, "BUY")
-
-    def limit_sell(self, scoin, ammount, bcoin, price):
-        return self.__limit_order(bcoin, ammount, scoin, price, "SELL")
-    # LIMIT ORDERS END
-
-    def _get_symbols_fill_filter(self):
-        l = []
-        exchange_info = self._get_no_sign("%s/exchangeInfo" % (self.BASE_URL_V3))
-        for symbol_dict in exchange_info['symbols']:
-            symbol = symbol_dict['symbol']
-            l.append(symbol)
-            tick = step = 0.0
-            for filter_dict in symbol_dict['filters']:
-                if filter_dict['filterType'] == 'PRICE_FILTER':
-                    tick = float(filter_dict['tickSize'])
-                elif filter_dict['filterType'] == 'LOT_SIZE':
-                    step = float(filter_dict['stepSize'])
-            BinanceAPI.TICK_STEP_DICT[symbol] = (tick, step)
-        return l
-
-    def _get_no_sign(self, path, params={}):
-        query = urlencode(params)
-        url = "%s?%s" % (path, query)
-        return requests.get(url, timeout=30, verify=True).json()
-    
-    def _sign(self, params={}):
-        data = params.copy()
-        ts = int(1000 * time.time())
-        data.update({"timestamp": ts})
-        h = urlencode(data)
-        b = bytearray()
-        b.extend(self.secret.encode())
-        signature = hmac.new(b, msg=h.encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
-        data.update({"signature": signature})
-        return data
-
-    def _get(self, path, params={}):
-        params.update({"recvWindow": config.recv_window})
-        query = urlencode(self._sign(params))
-        url = "%s?%s" % (path, query)
-        header = {"X-MBX-APIKEY": self.key}
-        return requests.get(url, headers=header, \
-            timeout=30, verify=True).json()
-
-    def _post(self, path, params={}):
-        params.update({"recvWindow": config.recv_window})
-        query = urlencode(self._sign(params))
-        header = {"X-MBX-APIKEY": self.key}
-        return requests.post(path, headers=header, data=query, \
-            timeout=30, verify=True).json()
-    
-    def _delete(self, path, params={}):
-        params.update({"recvWindow": config.recv_window})
-        query = urlencode(self._sign(params))
-        header = {"X-MBX-APIKEY": self.key}
-        return requests.delete(path, headers=header, data=query, \
-            timeout=30, verify=True).json()
-
-    def _order(self, symbol, ammount, side, ordertype, price = None):
-        # FILTER FAILURE: LOT_SIZE problem (step_size)
-        # FILTER FAILURE: PRICE_FILTER problem (tick_size)
-        def fix_value_to_pass_filters(val, correction):
-            integer_value = int(val / correction)
-            return integer_value * correction
-        
-        (tick, step) = BinanceAPI.TICK_STEP_DICT[symbol]
-        # For MARKET_FOR order type precision of ammount is price precision
-        if ordertype == self.OrderType.MARKET_FOR:
-            ammount = fix_value_to_pass_filters(ammount, tick)
-        else:
-            ammount = fix_value_to_pass_filters(ammount, step)
-        
-        # For limit order we have to adjust price to filters
-        if price is not None:
-            price = fix_value_to_pass_filters(price, tick)
-        
-        params = {}
-        params["type"] = "MARKET"
-
-        if ordertype == self.OrderType.LIMIT:
-            params["type"] = "LIMIT"
-            params["price"] = "%f" % (price)
-            params["timeInForce"] = "GTC"
-            params["quantity"] = "%f" % (ammount)
-        elif ordertype == self.OrderType.MARKET_FOR:
-            params["quoteOrderQty"] = "%f" % (ammount)
-        else:
-            params["quantity"] = "%f" % (ammount)
-        
-        params["symbol"] = symbol
-        params["side"] = side
+        if order_type == OrderType.LIMIT:
+            params['type'] = "LIMIT"
+            params['timeInForce'] = "GTC"
+            params['price'] = self._fix_value_to_pass_filters(price, price_tick)
 
         return params
+
+    def _fix_value_to_pass_filters(self, val, correction):
+        val = str(val)
+        dot = val.find('.')
+        if dot == -1:
+            return val + '.' + '0' * (len(correction) - 2)
+        if correction[0] == '1':
+            return val[:dot + 1] + '0' * (len(correction) - 2)
+        after_dot_val = val[dot + 1:]
+        after_dot_correction = correction[2:]
+        one_pos = after_dot_correction.find('1')
+        if one_pos >= len(after_dot_val) - 1:
+            after_dot_val += '0' * (len(after_dot_correction) - len(after_dot_val))
+        else:
+            after_dot_val = after_dot_val[:one_pos + 1] + '0' * (len(after_dot_correction) - one_pos - 1)
+        return val[:dot + 1] + after_dot_val
+
+    def __process_template(self, template):
+        for line in template.split("\n"):
+            if self.__line_is_necessary(line):
+                line_words = line.split(" ")
+                header = line_words[0][:-1]
+                header_content = line[len(header) + 2:]
+                self.headers[header] = header_content
+
+    def __line_is_necessary(self, line):
+        return (line.find("Referer") == -1 and
+                line.find("Content-Length") == -1 and
+                line.find("Origin") == -1 and
+                line.find("Host") == -1 and
+                line.find("POST") == -1)
+
+    def __fill_symbols_dicts(self):
+        all_symbols = []
+        exchange_info = self._get("%s/exchangeInfo" % BinanceAPI.BASE_API_URL)
+        for symbol_dict in exchange_info['symbols']:
+            symbol = symbol_dict['symbol']
+            all_symbols.append(symbol)
+            size_tick = price_tick = 0.0
+            for filter_dict in symbol_dict['filters']:
+                if filter_dict['filterType'] == 'PRICE_FILTER':
+                    price_tick = filter_dict['tickSize']
+                elif filter_dict['filterType'] == 'LOT_SIZE':
+                    size_tick = filter_dict['stepSize']
+            BinanceAPI.TICK_STEP_DICT[symbol] = (size_tick, price_tick)
+        BinanceAPI.COINS_SYMBOLS = frozenset(all_symbols)
--- config.py
+++ config.py
@@ -1,17 +1,19 @@
-# Profit in percent(e.g if you going to pump 100$
-# and would like to have 150$ then you should write 50)
-expected_profit = 50
+import keys
+
+# Pump
+expected_profit = 50  # Profit in percent. For example if you want make 150$ from 100$ set to 50.
+coin = "BUSD"
+coins_to_pump = 11
 
 # Binance config
-binance_api_key = "BIN_API_KEY"
-binance_api_secret = "BIN_API_SECRET"
-coin = "BUSD"
-coins_to_pump = 1000
+binance_api_key = keys.BINANCE_API_KEY
+binance_api_secret = keys.BINANCE_API_SECRET
+binance_header = keys.BINANCE_TRADE_HEADER
 
 # Telegram config
-telegram_api_id = 123
-telegram_api_hash = "API_HASH"
+telegram_api_id = keys.TELEGRAM_API_ID
+telegram_api_hash = keys.TELEGRAM_API_HASH
 telegram_chat_name = "Patryk Pęczak"
 
-# Better not to change
+# Don't change
 recv_window = 5000
